48713ea9b1a91b23d00b86c87640d4bdd43b04c6
Valentine <valentyn001@gmail.com>
2023-10-02 17:29:50 +0300

    0.0.1

---
+++ Editor/TexRampImporter.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.IO;
+using UnityEditor.AssetImporters;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+namespace nickeltin.TexRamp.Editor
+{
+    /// <summary>
+    /// TODO: Sprite support
+    /// TODO: public static context api to generate unity assets from *.texramp and metadata
+    /// </summary>
+    [ScriptedImporter(VERSION, EXT)]
+    internal class TexRampImporter : ScriptedImporter
+    {
+        public enum Compression
+        {
+            Disabled,
+            HighQuality,
+            LowQuality
+        }
+        
+        public const string EXT = "texramp";
+        public const int VERSION = 2;
+        
+        [SerializeField] internal int _majorResolution = 256;
+        [SerializeField] internal int _minorResolution = 32;
+        [SerializeField] internal Orientation _orientation = Orientation.Horizontal;
+        [SerializeField] internal TextureWrapMode _wrapMode = TextureWrapMode.Repeat;
+        [SerializeField] internal FilterMode _filterMode = FilterMode.Bilinear;
+        [SerializeField] internal Compression _compression = Compression.HighQuality;
+        
+        public override void OnImportAsset(AssetImportContext ctx)
+        {
+            var w = _majorResolution;
+            var h = 1;
+
+            if (_orientation == Orientation.Vertical)
+            {
+                w = 1;
+                h = _majorResolution;
+            }
+
+            TextureFormat texFormat;
+            var ramp = TexureRampSerialization.Load(assetPath);
+            switch (ramp.Format)
+            {
+                case Format.SingleChannel:
+                    texFormat = ramp.UseAlphaChannel ? TextureFormat.Alpha8 : TextureFormat.R8;
+                    break;
+                case Format.RG:
+                    texFormat = TextureFormat.RG16;
+                    break;
+                case Format.RGB:
+                    texFormat = TextureFormat.RGB24;
+                    break;
+                case Format.RGBA:
+                case Format.Gradient:
+                    texFormat = TextureFormat.RGBA32;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            
+            var oneDTex = new Texture2D(w,h, texFormat, false)
+            {
+                wrapMode = _wrapMode,
+                filterMode = _filterMode
+            };
+            
+            var pixels = new Color32[_majorResolution];
+            var step = 1f / (_majorResolution - 1);
+            for (var i = 0; i < _majorResolution; i++)
+            {
+                pixels[i] = ramp.Evaluate(step * i);
+            }
+
+            oneDTex.SetPixels32(pixels);
+            oneDTex.Apply();
+            
+            w = _majorResolution;
+            h = _minorResolution;
+
+            if (_orientation == Orientation.Vertical)
+            {
+                w = _minorResolution;
+                h = _majorResolution;
+            }
+
+            var twoDTex= Blit(oneDTex, w, h);
+
+            var notCompressable = ramp.Format == Format.SingleChannel;
+            if (!notCompressable)
+            {
+                switch (_compression)
+                {
+                    case Compression.Disabled:
+                        break;
+                    case Compression.HighQuality:
+                        twoDTex.Compress(true);
+                        break;
+                    case Compression.LowQuality:
+                        twoDTex.Compress(false);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            
+            twoDTex.Apply();
+            DestroyImmediate(oneDTex);
+
+
+            twoDTex.name = Path.GetFileNameWithoutExtension(assetPath) + "(Generated)";
+            ctx.AddObjectToAsset("TexRamp", twoDTex);
+            ctx.SetMainObject(twoDTex);
+        }
+
+        public static Texture2D Blit(Texture2D tex, int newWidth, int newHeight)
+        {
+            return Blit(tex, newWidth, newHeight, tex.format);
+        }
+        
+        public static Texture2D Blit(Texture2D tex, int newWidth, int newHeight, TextureFormat format)
+        {
+            var blitTex = new Texture2D(newWidth, newHeight, format, false)
+            {
+                wrapMode = tex.wrapMode,
+                filterMode = tex.filterMode,
+            };
+            
+            if (tex.format == TextureFormat.RGBA32)
+            {
+                blitTex.alphaIsTransparency = true;
+            }
+
+            var rtDesc = new RenderTextureDescriptor(newWidth, newHeight, GraphicsFormat.R8G8B8A8_UNorm, 0, 0);
+            var rt = RenderTexture.GetTemporary(rtDesc);
+            rt.wrapMode = tex.wrapMode;
+            rt.filterMode = tex.filterMode;
+            
+            // Just in case blitting with black tex
+            Graphics.Blit(Texture2D.blackTexture, rt);
+            Graphics.Blit(tex, rt);
+            RenderTexture.active = rt;
+            blitTex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+            RenderTexture.active = null;
+            RenderTexture.ReleaseTemporary(rt);
+            
+            return blitTex;
+        }
+    }
+}
---
+++ Editor/TextureRamp.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using UnityEngine;
+
+namespace nickeltin.TexRamp.Editor
+{
+    /// <summary>
+    /// Serializable wrapper with texture curves and other settings to generate texture from it
+    /// </summary>
+    [Serializable]
+    public struct TextureRamp
+    {
+        public Format Format;
+        public bool UseAlphaChannel;
+        public Gradient Gradient;
+        public TextureChannel RChannel;
+        public TextureChannel GChannel;
+        public TextureChannel BChannel;
+        public TextureChannel AChannel;
+
+        public TextureRamp(Format format,
+            Gradient gradient, bool useAlphaChannel,
+            TextureChannel rChannel, TextureChannel gChannel, 
+            TextureChannel bChannel, TextureChannel aChannel)
+        {
+            Format = format;
+            Gradient = gradient;
+            RChannel = rChannel;
+            GChannel = gChannel;
+            BChannel = bChannel;
+            AChannel = aChannel;
+
+            UseAlphaChannel = useAlphaChannel;
+        }
+
+        public static TextureRamp Default
+        {
+            get
+            {
+                var grad = new Gradient();
+                grad.SetKeys(new []
+                {
+                    new GradientColorKey(Color.white, 0), 
+                    new GradientColorKey(Color.white, 0.5f), 
+                    new GradientColorKey(Color.white, 1)
+                    
+                }, new []
+                {
+                    new GradientAlphaKey(0, 0),
+                    new GradientAlphaKey(1, 0.5f),
+                    new GradientAlphaKey(0, 1),
+                });
+                
+                return new TextureRamp(Format.SingleChannel, grad, false,
+                    TextureChannel.Default, TextureChannel.Default,
+                    TextureChannel.Default, TextureChannel.Default);
+            }
+        }
+
+
+        public readonly Color Evaluate(float t)
+        {
+            t = Mathf.Clamp01(t);
+            if (Format == Format.Gradient)
+            {
+                return Gradient.Evaluate(t);
+            }
+
+            float EvalChannel(TextureChannel channel)
+            {
+                var tL = t;
+                if (channel.Inverted) tL = 1 - tL;
+                return channel.Curve.Evaluate(tL);
+            }
+
+            var r = EvalChannel(RChannel);
+            var g = EvalChannel(GChannel);
+            var b = EvalChannel(BChannel);
+            var a = EvalChannel(AChannel);
+
+            return new Color(r, g, b, a);
+        }
+    }
+
+    [Serializable]
+    public struct TextureChannel
+    {
+        public AnimationCurve Curve;
+        public bool Inverted;
+
+        public TextureChannel(AnimationCurve curve, bool inverted)
+        {
+            Curve = curve;
+            Inverted = inverted;
+        }
+
+        public static TextureChannel Default
+        {
+            get
+            {
+                return new TextureChannel(new AnimationCurve(new []
+                {
+                    new Keyframe(0, 0),
+                    new Keyframe(0.5f, 1),
+                    new Keyframe(1, 0)
+                }), false);
+            }
+        }
+    }
+    
+    public enum Orientation
+    {
+        Horizontal, Vertical
+    }
+    
+    public enum Format
+    {
+        SingleChannel,
+        RG,
+        RGB,
+        RGBA,
+        Gradient
+    }
+}
